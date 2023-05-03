a7de691102d949993da05074e410f57067ec8c92
NateDog-codes <128638625+NateDog-codes@users.noreply.github.com>
2023-05-03 13:00:24 -0700

    Update index.js

--- Encryptor/index.js
+++ Encryptor/index.js
@@ -1,1 +1,10 @@
 console.log('The encryptor js file loaded');
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+
+console.log(letters[0]); 
+
+for (let i = 0; i < letters.legth; i++)
+  console.log(letters[i]);
+}
