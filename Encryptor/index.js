ccd035b493ea71306a886dd840e20a3842f1877d
NateDog-codes <128638625+NateDog-codes@users.noreply.github.com>
2023-05-10 12:53:54 -0700

    Update index.js

--- Encryptor/index.js
+++ Encryptor/index.js
@@ -2,13 +2,25 @@ console.log('The encryptor js file loaded');
 
 const letters = 'abcdefghijklmnopqrstuvwxyz';
 const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
-let sum = 0;
 
-for (let i = 1; i < 10; i++) {
-console.log(letters[0]); 
-sum = sum + i;
-  console.log('sum: '+ sum); 
+function encrypt(text) {
+let result = '';
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i]; 
+    let letterIndex = letters.indexOf(letter);
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }
+    else {
+      result += encrypt_key[letterIndex];
+  }
+ }
+  
+  return result;
 }
+  
 
 
   
