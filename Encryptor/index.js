7547f7184064eaac1fa796870f2334793eafd21b
NateDog-codes <128638625+NateDog-codes@users.noreply.github.com>
2023-05-10 13:01:24 -0700

    Update index.js

--- Encryptor/index.js
+++ Encryptor/index.js
@@ -1,7 +1,7 @@
 console.log('The encryptor js file loaded');
 
-const letters = 'abcdefghijklmnopqrstuvwxyz';
-const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+const letters = 'abcdefghijklmnopqrstuvwxyz!?ABCDEFGHIJKLMNOPQRSTUVWXYZ ';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza?!BCDEFGHIJKLMNOPQRSTUVWXYZA ';
 
 function encrypt(text) {
 let result = '';
@@ -15,6 +15,26 @@ let result = '';
     }
     else {
       result += encrypt_key[letterIndex];
+  }
+ }
+  
+  return result;
+}
+  
+
+//Decrypt 
+function Decrypt(text) {
+let result = '';
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i]; 
+    let letterIndex = letters.indexOf(letter);
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }
+    else {
+      result += letters[letterIndex];
   }
  }
   
