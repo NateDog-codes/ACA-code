f0ddce119fa7123a9cefda8a0898c1f95d40ba12
NateDog-codes <128638625+NateDog-codes@users.noreply.github.com>
2023-05-24 12:16:50 -0700

    Update index.js

--- Encryptor/index.js
+++ Encryptor/index.js
@@ -20,9 +20,31 @@ let result = '';
   
   return result;
 }
+
+
+function decrypt(text) {
+  let result = '';
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
+    let letterIndex = encrypt_key.indexOf(letter);
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }// if
+    
+    else {
+      result += letters[letterIndex]; 
+    }// else
+    
+  }// for
+  
+  return result;
+  
+}// decrypt
   
 
-//Decrypt 
+ 
 
 const encryptForm = document.getElementById('encrypt-form')
 encryptForm.addEventListener('submit', function(e) {
@@ -57,6 +79,42 @@ let result = '';
 }
   
 
+
+
+
+
+
+
+const decryptForm = document.getElementById('decrypt-form')
+decryptForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  //get the value of the content
+  let messageElement = document.getElementById('decrypt');
+  let message = messageElement.value;
+  
+
+    let decrypted = decrypt(message);
+  
+  let decodedMessageElement = document.getElementById('decoded-message');
+  decodedMessageElement.innerText = decrypted; 
+          }); 
+
+function Decrypt(text) {
+let result = '';
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i]; 
+    let letterIndex = letters.indexOf(letter);
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }
+    else {
+      result += letters[letterIndex];
+  }
+ }
+  
+  return result;
 
   
 
